aeff5fab82076543c6e750269b990043b44e1853
Asmaa <asmaa.alamir456@gmail.com>
2022-10-07 21:50:06 +0200

    first commit

---
+++ Producer/Sender.cs
@@ -0,0 +1,25 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace Producer
+{
+    public class Sender
+    {
+        public static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare("BasicTest", false, false, false, null);
+                string message = "Getting started with .net core RabbitMQ";
+                var body = Encoding.UTF8.GetBytes(message);
+                channel.BasicPublish("", "BasicTest", null, body);
+                Console.WriteLine("sent message", message);
+            }
+            Console.WriteLine("press enter to exist");
+            Console.ReadLine();
+        }
+    }
+}
